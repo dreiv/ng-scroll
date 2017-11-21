fc2832df9b4229bb42a8e3de932fc5f71bc31eed
dreiv <dreivoicu@gmail.com>
2017-11-21 21:11:22 +0200

    testing build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>hello world!</h1>`,
+  template: `<h1>hello world</h1>`,
   styles: []
 })
 export class AppComponent {
