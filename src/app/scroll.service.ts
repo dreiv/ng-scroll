20baf6f216ca2e3a3d5bbbc0a88268fa842329c2
Andrei Voicu <Andrei-Florian.Voicu@haufe-lexware.com>
2017-12-15 11:49:34 +0200

    filler content.

--- src/app/scroll.service.ts
+++ src/app/scroll.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, NgZone } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
-import { passiveSupported } from './utils';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 import { fromEvent } from 'rxjs/observable/fromEvent';
@@ -9,42 +8,46 @@ import { auditTime } from 'rxjs/operators';
 /** Time in ms to throttle the scrolling events by default. */
 export const DEFAULT_SCROLL_TIME = 20;
 
+const previousPosition: number = -1;
+
 @Injectable()
 export class ScrollService {
-  /** Keeps track of the global `scroll` and `resize` subscriptions. */
-  _globalSubscription: Subscription | null = null;
+  /** Keeps track of the global `scroll` subscription. */
+  globalSubscription: Subscription | null;
 
   /** Subject for notifying that a registered scrollable reference element has been scrolled. */
   private _scrolled = new Subject<void>();
 
-  constructor(private ngZone: NgZone) {
-  }
+  constructor(private ngZone: NgZone) {}
 
-  scrolled$(auditTimeInMs: number = DEFAULT_SCROLL_TIME): Observable<void> {
+  scrolled$(auditTimeInMs: number = DEFAULT_SCROLL_TIME): Observable<string> {
     return Observable.create(observer => {
-      if (!this._globalSubscription) {
-        this._addGlobalListener();
+      if (!this.globalSubscription) {
+        this.addGlobalListener();
       }
 
       // In the case of a 0ms delay, use an observable without auditTime
       // since it does add a perceptible delay in processing overhead.
-      if (auditTimeInMs > 0){
-        this._scrolled.pipe(auditTime(auditTimeInMs)).subscribe(observer);
-      } else {
-        this._scrolled.subscribe(observer);
-      }
+      const subscription = auditTimeInMs > 0
+        ? this._scrolled.pipe(auditTime(auditTimeInMs)).subscribe(() => {
+              this.ngZone.run(() => {
+                  observer.next(`${document.scrollingElement.scrollTop}`);
+              });
+          })
+        : this._scrolled.subscribe(observer);
     });
   }
 
-  /** Sets up the global scroll and resize listeners. */
-  private _addGlobalListener() {
-    const eventOptions = passiveSupported
-      ? { capture: true, passive: true }
-      : true;
+  /** Sets up the global scroll listener. */
+  private addGlobalListener() {
+    this.globalSubscription = this.ngZone.runOutsideAngular(() => {
+      return fromEvent(window.document, 'scroll', { passive: true })
+        .subscribe((event: any) => {
+          const el = event.currentTarget.scrollingElement;
 
-    this._globalSubscription = this.ngZone.runOutsideAngular(() => {
-      return fromEvent(window.document, 'scroll', eventOptions)
-        .subscribe(() => this._scrolled.next());
+          this._scrolled.next();
+          console.log('top: ', el.scrollTop);
+        });
     });
   }
 }
--- src/app/utils.ts
+++
@@ -1,9 +0,0 @@
-try {
-  const options = Object.defineProperty({}, "passive", {
-    get: () => passiveSupported = true
-  });
-
-  window.addEventListener("test", null, options);
-} catch(err) {}
-
-export let passiveSupported: boolean;
