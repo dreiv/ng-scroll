1d0a22dd095591f466667a299e3b695e3d7a1d5e
Andrei Voicu <Andrei-Florian.Voicu@haufe-lexware.com>
2017-12-18 09:57:37 +0200

    cleanup.

--- src/app/scroll.service.ts
+++ src/app/scroll.service.ts
@@ -1,13 +1,13 @@
 import { Injectable, NgZone } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { Subject } from 'rxjs/Subject';
-import { Subscriber } from 'rxjs/Subscriber';
-import { Subscription } from 'rxjs/Subscription';
 import { fromEvent } from 'rxjs/observable/fromEvent';
+import { Subject } from 'rxjs/Subject';
+import { Subscription } from 'rxjs/Subscription';
 import { auditTime } from 'rxjs/operators';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/share';
 import { isIOS } from './utils';
+import 'rxjs/add/operator/mapTo';
 
 export enum ScrollDirection {
     UP = 'up',
@@ -20,12 +20,12 @@ export const DEFAULT_SCROLL_TIME = 20;
 @Injectable()
 export class ScrollService {
   /** Keeps track of the global `scroll` subscription. */
-  globalSubscription: Subscription | null;
+  globalSubscription: Subscription;
 
   /** Subject for notifying that a registered scrollable reference element has been scrolled. */
   public scrolled: Subject<Element> = new Subject<Element>();
-  private previousScrollTop = 0;
-  private previousScrollingElementWidth: number;
+  private previousScrollTop: number;
+  private previousOffsetHeight: number;
 
   constructor(private ngZone: NgZone) {}
 
@@ -37,43 +37,32 @@ export class ScrollService {
 
       // In the case of a 0ms delay, use an observable without auditTime
       // since it does add a perceptible delay in processing overhead.
-      if (auditTimeInMs > 0) {
-          this.scrolled.pipe(auditTime(auditTimeInMs))
-              .subscribe((scrollingElement: HTMLElement) => {
-                  this.resolveDirection(observer, scrollingElement);
-              });
-      } else {
-          this.scrolled
-              .subscribe((scrollingElement: HTMLElement) => {
-                  this.resolveDirection(observer, scrollingElement);
-              });
-      }
-    }).distinctUntilChanged()
-      .share();
-  }
+      const subscription = auditTimeInMs > 0
+        ? this.scrolled.pipe(auditTime(auditTimeInMs))
+        : this.scrolled;
 
-  private resolveDirection(observer: Subscriber<ScrollDirection>, scrollingElement: HTMLElement) {
-    const scrollingElementWidth = scrollingElement.clientWidth;
+      subscription.subscribe((scrollingElement: HTMLElement) => {
+        const currentScrollTop = isIOS()
+          ? Math.max(Math.min(scrollingElement.scrollTop, scrollingElement.scrollHeight - window.innerHeight), 0)
+          : scrollingElement.scrollTop;
+        const offsetHeight = scrollingElement.offsetHeight;
 
-    if (this.previousScrollingElementWidth === scrollingElementWidth) {
-      const currentScrollTop = isIOS()
-        ? Math.max(Math.min(scrollingElement.scrollTop, scrollingElement.scrollHeight - window.innerHeight), 0)
-        : scrollingElement.scrollTop;
+        if (offsetHeight === this.previousOffsetHeight && currentScrollTop !== this.previousScrollTop) {
+          const direction: ScrollDirection = currentScrollTop > this.previousScrollTop
+            ? ScrollDirection.DOWN
+            : ScrollDirection.UP;
 
-      if (currentScrollTop !== this.previousScrollTop) {
-        const direction: ScrollDirection = currentScrollTop > this.previousScrollTop
-          ? ScrollDirection.DOWN
-          : ScrollDirection.UP;
+          this.ngZone.run(() => {
+            observer.next(direction);
+          });
+        }
 
         this.previousScrollTop = currentScrollTop;
+        this.previousOffsetHeight = offsetHeight;
+      });
 
-        this.ngZone.run(() => {
-          observer.next(direction);
-        });
-      }
-    }
-
-    this.previousScrollingElementWidth = scrollingElementWidth;
+    }).distinctUntilChanged()
+      .share();
   }
 
   /** Sets up the global scroll listener. */
